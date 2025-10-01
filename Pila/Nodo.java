22b027fc23b9730b982dc81ad4736f53089c8cb4
Mateo-Gomez-Marulanda <mateo.gomezm@uqvirtual.edu.co>
2025-10-01 08:29:33 -0500

    creacion de Bicola y pila

---
+++ Pila/Nodo.java
@@ -0,0 +1,39 @@
+package co.edu.uniquindio;
+
+public class Nodo<T> {
+    private T dato;
+    private Nodo<T> proximo; //Referencia al siguiente elemento
+
+    public Nodo(T dato) {
+        this.dato = dato;
+        proximo = null; //Null inicialmente ya que no referencia a nada antes de estar en la lista
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public Nodo<T> getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Nodo<T> proximo) {
+        this.proximo = proximo;
+    }
+
+    @Override
+    public String toString() {
+        return "Nodo{" +
+                "dato=" + dato +
+                ", proximo=" + proximo +
+                '}';
+    }
+
+    public void setAnterior(Nodo<T> actual) {
+        this.proximo = actual;
+    }
+}
